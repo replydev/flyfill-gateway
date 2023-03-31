d9c48cab5cb40fc8f4cb0aa89561a088782a4ae1
replydev <commoncargo@tutanota.com>
2023-03-31 19:48:35 +0200

    Test missing session id case

--- src/test/java/org/flyfill/controller/SessionHandlerTest.java
+++ src/test/java/org/flyfill/controller/SessionHandlerTest.java
@@ -5,9 +5,11 @@ import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.flyfill.dto.PasswordFillRequestDTO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import javax.websocket.*;
+import java.io.IOException;
 import java.net.URI;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
@@ -26,9 +28,11 @@ class SessionHandlerTest {
     URI uri;
 
     @Test
-    public void testPasswordFill() throws Exception {
+    @DisplayName("Test password fill with correct session id")
+    void testPasswordFill() throws Exception {
 
-        PasswordFillRequestDTO payload = PasswordFillRequestDTO.builder().sessionId("1234").encryptedUsername("test").encryptedPassword("test").build();
+        String correctSessionId = "1234";
+        PasswordFillRequestDTO payload = PasswordFillRequestDTO.builder().sessionId(correctSessionId).encryptedUsername("test").encryptedPassword("test").build();
 
         try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(Client.class, uri)) {
             assertEquals("CONNECT", MESSAGES.poll(10, TimeUnit.SECONDS));
@@ -48,6 +52,28 @@ class SessionHandlerTest {
             PasswordFillRequestDTO obtainedPayload = new ObjectMapper().readValue(responseFromRequestDispatcher, PasswordFillRequestDTO.class);
 
             assertEquals(obtainedPayload, payload);
+        }
+    }
+
+    @Test
+    @DisplayName("Test password fill with non existent session id")
+    void testMissingSessionId() throws DeploymentException, IOException, InterruptedException {
+        String correctSessionId = "1235";
+        PasswordFillRequestDTO payload = PasswordFillRequestDTO.builder().sessionId(correctSessionId).encryptedUsername("test").encryptedPassword("test").build();
+
+        try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(Client.class, uri)) {
+            assertEquals("CONNECT", MESSAGES.poll(10, TimeUnit.SECONDS));
+
+            // Send POST request from the android device
+            given()
+                    .when()
+                    .contentType(ContentType.JSON)
+                    .body(payload)
+                    .post("/fill/queue")
+                    .then()
+                    .statusCode(404)
+                    .body("message", is("Session id not found: 1235"))
+                    .body("status", is(404));
         }
     }
 
